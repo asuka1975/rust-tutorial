134a8b8f75bab0fc3235bf4a4e8f80681a41973b
hungr <hungryfactory@icloud.com>
2023-06-29 21:25:53 +0900

    feat: finish variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,18 @@
+const MAX_POINTS: u32 = 100_000;
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let x = 5;
+
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
+    println!("The value of x is: {}", x);
 }
